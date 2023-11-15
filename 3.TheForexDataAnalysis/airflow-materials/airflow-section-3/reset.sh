da5a1093ade23c8a9cdfedb4ea34c2543e299c9c
Nitish <22993803+nitish-raj@users.noreply.github.com>
2023-11-15 20:43:29 +0000

    update for docker compose

--- 3.TheForexDataAnalysis/airflow-materials/airflow-section-3/reset.sh
+++ 3.TheForexDataAnalysis/airflow-materials/airflow-section-3/reset.sh
@@ -1,6 +1,6 @@
 # /!\ WARNING: RESET EVERYTHING! 
 # Remove all containers/networks/volumes/images and data in db
-docker-compose down
+docker compose down
 docker system prune -f
 docker volume prune -f
 docker network prune -f
