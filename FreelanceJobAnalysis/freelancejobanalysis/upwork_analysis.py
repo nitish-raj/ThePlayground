968e5e75fe49de05d587f70041038ea94b400ab9
Nitish <22993803+nitish-raj@users.noreply.github.com>
2023-12-27 19:51:32 +0000

    Adding logical changes

--- FreelanceJobAnalysis/freelancejobanalysis/upwork_analysis.py
+++ FreelanceJobAnalysis/freelancejobanalysis/upwork_analysis.py
@@ -1,4 +1,3 @@
-#%%
 import pandas as pd
 import feedparser
 import re
@@ -37,7 +36,7 @@ def CleanHTML(results):
     res = re.sub("</wbr>", "", res)
     res = re.sub("&lt;", "", res)
     res = re.sub("<.*?>", "", res)
-    res = re.sub("&nbsp;", "", res)
+    res = re.sub("&nbsp;|&amp;", "", res)
     res = re.sub("&bull;", "", res)
     res = re.sub("\n", "", res)
     res = re.sub("  ", "", res)
@@ -47,36 +46,75 @@ def CleanHTML(results):
 def extract_value(paragraphs, index, part):
     """Extract value from paragraphs list or return 'NA' if IndexError"""
     try:
-        if index == -3:
-            return re.sub(
-                ": |:|\t", "", CleanHTML(paragraphs[index].split("</b>")[part])
-            )
-        return (re.sub(": |:", "", CleanHTML(paragraphs[index].split("</b>")[part])),)
+        return (
+            re.sub(
+                ": |:|\t",
+                "",
+                CleanHTML(
+                    paragraphs[index].split("</b>")[part])),
+        )
     except IndexError:
-        return "NA"
+        return ("",)
 
-#%%
+
 def texttodf(feed) -> List[List[str]]:
     temp = []
-    try:
-        for entry in feed.entries:
-            paragraphs = (entry.summary).split("<br />")
+
+    columns = [
+        "Budget",
+        "Posted On",
+        "Category",
+        "Country",
+        "Hourly Range",
+        "Skills",
+    ]
+
+    for entry in feed.entries:
+        paragraphs = (entry.summary).split("<br />")
+        try:
+            col_idx = {}
+            for i, x in enumerate(paragraphs):
+                for col in columns:
+                    if re.search(col, x):
+                        col_idx[col] = i
+                        break
+
             temp.append(
                 [
-                    CleanHTML(entry.title),
+                    CleanHTML(entry.title[:-9]),
                     entry.link,
                     entry.updated,
-                    extract_value(paragraphs, -5, 1)[0],
+                    extract_value(
+                        paragraphs, col_idx.get(columns[1]) or len(paragraphs), 1
+                    )[0],
                     CleanHTML(entry.summary),
-                    extract_value(paragraphs, -6, 1)[0],
-                    extract_value(paragraphs, -6, 0)[0],
-                    extract_value(paragraphs, -4, 1)[0],
-                    extract_value(paragraphs, -3, 1),
-                    extract_value(paragraphs, -2, 1)[0],
+                    extract_value(
+                        paragraphs,
+                        col_idx.get(columns[0])
+                        or col_idx.get(columns[4])
+                        or len(paragraphs),
+                        1,
+                    )[0],
+                    extract_value(
+                        paragraphs,
+                        col_idx.get(columns[0])
+                        or col_idx.get(columns[4])
+                        or len(paragraphs),
+                        0,
+                    )[0],
+                    extract_value(
+                        paragraphs, col_idx.get(columns[2]) or len(paragraphs), 1
+                    )[0],
+                    extract_value(
+                        paragraphs, col_idx.get(columns[5]) or len(paragraphs), 1
+                    )[0],
+                    extract_value(
+                        paragraphs, col_idx.get(columns[3]) or len(paragraphs), 1
+                    )[0],
                 ]
             )
-    except Exception as e:
-        logging.error(f"{e} error while processing entry: {paragraphs}")
+        except Exception as e:
+            logging.error(f"{e} error while processing entry: {entry.summary}")
 
     return temp
 
@@ -94,32 +132,36 @@ def main():
         "Skills",
         "Country",
     ]
-    data = pd.DataFrame(columns=columns)
+    data_list = []
 
     for url in config_object["FEEDS"]["url"].split("\n"):
         try:
             feed = feedparser.parse(url)
-            data = pd.concat(
-                [data, pd.DataFrame(texttodf(feed), columns=columns)], ignore_index=True
-            )
+            data_list.extend(texttodf(feed))
         except Exception as e:
             logging.error(f"Error parsing feed '{url}': {e}")
+
+    data = pd.DataFrame(data_list, columns=columns)
 
     try:
         data["Publish_Date"] = pd.to_datetime(
             data.Publish_Date, format="mixed", dayfirst=True
         )
     except Exception as e:
-        logging.error(f"Error converting 'Publish_Date' column to datetime: {e}")
+        logging.error(
+            f"Error converting 'Publish_Date' column to datetime: {e}")
 
     try:
         data["Update_Date"] = pd.to_datetime(
             data.Update_Date, format="mixed", dayfirst=True
         )
     except Exception as e:
-        logging.error(f"Error converting 'Update_Date' column to datetime: {e}")
+        logging.error(
+            f"Error converting 'Update_Date' column to datetime: {e}")
 
-    data.to_csv(f'{root_folder}/{config_object["EXPORT"]["file_name"]}', index=False)
+    data.to_csv(
+        f'{root_folder}/{config_object["EXPORT"]["file_name"]}',
+        index=False)
     logging.info("Data extraction and processing completed successfully.")
 
 
