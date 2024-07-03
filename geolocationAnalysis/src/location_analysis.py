37634985cf1b818cbca26d6fde21c79d2c6dcd0a
Nitish <22993803+nitish-raj@users.noreply.github.com>
2024-07-03 21:09:37 +0000

    feat: :construction: Adding GeoNames API to fetch Lat and Lon information

--- geolocationAnalysis/src/location_analysis.py
+++ geolocationAnalysis/src/location_analysis.py
@@ -6,6 +6,7 @@ import duckdb
 import pandas as pd
 from configparser import ConfigParser
 import sys
+import logging
 
 @dataclass
 class Place:
@@ -23,14 +24,64 @@ class PlaceDetail:
     phoneNumber: str
     website: str
 
+@dataclass
+class GeoName:
+    geonameid: int
+    name: str
+    latitude: float
+    longitude: float
+    country_code: str
+    admin1_code: Optional[str] = None
+    population: Optional[int] = None
+
+
+
 def format_coordinates(latitude, longitude):
     return f"{latitude},{longitude}"
+
+
+class GeoMapClient:
+    def __init__(self, username):
+        self.username = username
+
+    def get_geonames_data(self, country_code, max_rows=1000):
+        base_url = "http://api.geonames.org/searchJSON"
+        all_data = []
+        start_row = 0
+        
+        while True:
+            params = {
+                "country": country_code,
+                "maxRows": max_rows,
+                "username": self.username,
+                "startRow": start_row
+            }
+            
+            response = requests.get(base_url, params=params)
+            data = response.json()
+            
+            if "geonames" in data:
+                geonames = data["geonames"]
+                all_data.extend(geonames)
+                
+                if len(geonames) < max_rows:
+                    # We've reached the end of the data
+                    break
+                
+                start_row += max_rows
+                
+                # Respect rate limits
+                time.sleep(1)
+            else:
+                print("Error retrieving data:", data.get("status", {}).get("message", "Unknown error"))
+                return None
+        
 
 class GoogleMapsClient:
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def get_coordinates(self, address):
+    def get_coordinates_google(self, address):
         url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={self.api_key}"
         response = requests.get(url)
         if response.status_code == 200:
@@ -76,14 +127,19 @@ class GoogleMapsClient:
 
     def get_places_details(self, place_ids):
         all_place_details = []
-        
+
         for place_id in place_ids:
             details_url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&key={self.api_key}"
             details_response = requests.get(details_url)
             place_details = details_response.json()
-            all_place_details.extend(place_details['result'])
+            
+            if place_details['status'] != 'OK':
+                # print(f"Error in response: {place_details['status']}")
+                continue
+            
+            all_place_details.append(place_details['result'])
 
-        return [self._convert_to_placedetail(result) for result in all_place_details]
+        return [self._convert_to_placedetail(result) for result in all_place_details] 
 
     def _convert_to_placedetail(self, result):
         place_id = result['place_id']
@@ -94,7 +150,7 @@ class GoogleMapsClient:
         return PlaceDetail(place_id, name, address, phoneNumber, website)
 
 class DuckDBHandler:
-    def __init__(self, db_path='places.db', recreate: bool = False):
+    def __init__(self, db_path='../data/places.db', recreate: bool = False):
         self.con = duckdb.connect(db_path)
         self.recreate = recreate
         self._create_table(self.recreate)
@@ -127,17 +183,20 @@ class DuckDBHandler:
 
     def insert_places(self, places):
         data = [[place.place_id, place.name, place.vicinity, place.latitude, place.longitude] for place in places]
-        df = pd.DataFrame(data, columns=['place_id', 'name', 'address', 'latitude', 'longitude'])
+        df = pd.DataFrame(data, columns=['place_id', 'name', 'vicinity', 'latitude', 'longitude'])
         self.con.execute('BEGIN TRANSACTION')
         self.con.execute('INSERT INTO places SELECT * FROM df where place_id not in (select place_id from places group by 1)')
         self.con.execute('COMMIT')
     
     def insert_place_details(self, placedetail):
         data = [[detail.place_id, detail.name, detail.address, detail.phoneNumber, detail.website] for detail in placedetail]
-        df = pd.DataFrame(data)
+        df = pd.DataFrame(data, columns= ['place_id','name','address','phoneNumber', 'website'])
         self.con.execute('BEGIN TRANSACTION')
-        self.con.execute('INSERT INTO places SELECT * FROM df')
+        self.con.execute('INSERT INTO placeDetail SELECT * FROM df where place_id not in (select place_id from placeDetail group by 1)')
         self.con.execute('COMMIT')
+
+    def _insert_data(self, data, table_name):
+        self.con.execute(f'INSERT INTO {table_name} VALUES (?)', data)
 
 #%%
 if __name__ == "__main__":
@@ -156,17 +215,25 @@ if __name__ == "__main__":
     google_maps_client = GoogleMapsClient(API_KEY)
 
     # Fetch coordinates for the given address
-    latitude, longitude = google_maps_client.get_coordinates(address)
+    # latitude, longitude = google_maps_client.get_coordinates(address)
+
+    latitude, longitude = 
+
     if latitude and longitude:
         location = format_coordinates(latitude, longitude)
-        radius = sys.argv[2] if len(sys.argv) > 2 else 15000  # in meters
+        radius = sys.argv[2] if len(sys.argv) > 2 else 50000  # in meters
 
         # Fetch places and insert into DuckDB
         places = google_maps_client.get_all_places(location, radius)
+        PlaceDetails = google_maps_client.get_places_details([place.place_id for place in places])
+
+        # pass True to recreate the tables else False
+        duckdb_handler = DuckDBHandler(recreate=sys.argv[3] if len(sys.argv) > 3 else False)
+        
         if places:
-            duckdb_handler = DuckDBHandler(recreate=sys.argv[3] if len(sys.argv) > 3 else False) # pass True to recreate the table else False
             duckdb_handler.insert_places(places)
-            # duckdb_handler.insert_places(places)
+        if PlaceDetails:
+            duckdb_handler.insert_place_details(PlaceDetails)
         else:
             print("No places found.")
     else:
